e855ee692e1c4f5e128bb5aee69c3af2999cabd1
Katy Kane <katytheartist@gmail.com>
2024-02-01 20:32:15 -0300

    now its 15/17, what

--- mvpB.test.js
+++ mvpB.test.js
@@ -15,6 +15,24 @@ describe('Sprint 7 Challenge Learner Tests', () => {
     [5] sum('10', '3') // returns 13
   */
 
+    describe('unit tests for sum', ()=>{
+      it('should return "pass valid numbers" when both a and b are missing', ()=>{
+        expect(sum()).toBe('pass valid numbers')
+      })
+      it('should return "pass valid numbers" when a or b are not integers or strings of integers', ()=>{
+        expect(sum(2, 'seven')).toBe('pass valid numbers')
+      })
+      it('should return a + b when both a and b are integers', ()=>{
+        expect(sum(1, 3)).toBe(4)
+      })
+      it('should return a + b when  a and b are a combination of integers and strings containing integers', ()=>{
+        expect(sum('1', 2)).toBe(3)
+      })
+      it('should return a + b when both a and b are strings of integers', ()=>{
+        expect(sum('10', '3')).toBe(13)
+      })
+    })
+
   /*
   👉 TASK 2 - Integration Testing of HelloWorld component at the bottom of this module
 
@@ -29,9 +47,37 @@ describe('Sprint 7 Challenge Learner Tests', () => {
     [5] renders a text that reads "JavaScript is pretty awesome"
     [6] renders a text that includes "javaScript is pretty" (use exact = false)
   */
-  test('you can comment out this test', () => {
-    expect(true).toBe(false)
-  })
+  // test('you can comment out this test', () => {
+  //   expect(true).toBe(false)
+  // })
+   describe('RTL tests for HelloWorld', ()=>{
+    it('should render HelloWorld properly', ()=>{
+      render(<HelloWorld />)
+
+      const home = screen.queryByText('Home')
+      expect(home).toBeInTheDocument()
+      expect(home).toHaveAttribute('href')
+
+      const about = screen.queryByText('About')
+      expect(about).toBeInTheDocument()
+      expect(home).toHaveAttribute('href')
+
+      const blog = screen.queryByText('Blog')
+      expect(blog).toBeInTheDocument()
+      expect(home).toHaveAttribute('href')
+
+
+      const theTruth = screen.queryByText('The Truth')
+      expect(theTruth).toBeInTheDocument()
+
+      const awesome = screen.queryByText('JavaScript is pretty awesome')
+      expect(awesome).toBeInTheDocument()
+
+      const includes = screen.queryByText('javaScript is pretty', {exact: false})
+      expect(includes).toBeInTheDocument()
+    })
+   }) 
+
 })
 
 function sum(a, b) {
